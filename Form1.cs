55f4da3c7670f5f8a08f37469307e753e2f84f08
GeorgiyZhilinskiy <79874368+GeorgiyZhilinskiy@users.noreply.github.com>
2023-02-16 20:20:31 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,21 @@
+namespace Lab1_New
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += button1_Click;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
